899d7bf1999a8bfbedaeeb17a9ca0ceb5568586a
Mahmut İÇME <arrovain@gmail.com>
2024-07-03 17:28:08 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import './assets/main.css'
-import App from './App.vue'
-import { createApp } from 'vue'
 
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
 import router from './router'
-import jQuery from 'jquery'
-window.jQuery = jQuery
-window.$ = jQuery
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
