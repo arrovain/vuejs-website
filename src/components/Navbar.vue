3aac514a4293bfd3c6022e06f0969da9d4f2fbcf
Mahmut İÇME <arrovain@gmail.com>
2024-07-04 12:47:43 +0300

    type.icon and type.name for img

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -33,6 +33,8 @@
       <div class="slider-container">
         <div class="insurance-types">
           <div v-for="(type, index) in insuranceTypes :key="index" class="type-icon">
+            <img :src="type.icon" :alt"type.name">
+          </div>
 
         </div>
       </div>
