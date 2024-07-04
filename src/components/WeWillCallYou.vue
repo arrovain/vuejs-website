c07690684a8673f2019007e7da36fb9c76698852
Mahmut İÇME <arrovain@gmail.com>
2024-07-04 10:07:29 +0300

    style for insurance-banner

--- src/components/WeWillCallYou.vue
+++ src/components/WeWillCallYou.vue
@@ -37,6 +37,19 @@ export default {
 
 .insurance-banner {
     background-color: #f0f0f0;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 500;
+    align-items: center;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    border-radius: 20px;
+    unicode-bidi: isolate;
+
 }
 
 
