baf257b95bd67c8f2b4dcf391eb44ad5ab2017be
Mahmut İÇME <arrovain@gmail.com>
2024-07-04 11:45:09 +0300

    span for mail and phone

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,7 +3,8 @@
 <div id="app">
     <header>
         <div class="contact-info">
-            <span><i class="email-icon"></i> </span>
+            <span><i class="email-icon"> info@tercansigorta.net </i> </span>
+            <span><i class="phone-icon"> 0 505 112 78 44 </i> </span>
         </div>
     </header>
 </div>
