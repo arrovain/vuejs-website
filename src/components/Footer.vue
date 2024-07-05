689b0cc24701eae82d688d34d827aeae8342f09d
Mahmut İÇME <arrovain@gmail.com>
2024-07-05 11:58:02 +0300

    FOOTER AND FOOTER CONTENT

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<template>
+    <footer class="footer">
+      <div class="footer-content">
+        <div class="logo">
+          <span class="logo-text">TERCAN SİGORTA LOGO</span><span class="logo-24">24</span>
+          <span class="logo-slogan">Sigorta Aracılık Hizmetleri</span>
+        </div>
+        <div class="rights">
+          TERCAN SİGORTA © 2024 | Tüm hakları saklıdır.
+        </div>
+      </div>
+    </footer>
+  </template>
