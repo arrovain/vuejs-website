ffbfe423070deabf742d1b6109ba02d81f8ac737
Mahmut İÇME <arrovain@gmail.com>
2024-07-09 11:52:10 +0300

    index store and show function

--- backend/app/Http/Controllers/NewsController.php
+++ backend/app/Http/Controllers/NewsController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    //
+    public function index()
+    {
+        return News::all();
+    }
+
+    public function store(Request $request)
+    {
+        $news = News::create($request->all());
+        return response()->json($news, 201);
+    }
+
+    public function show($id)
+    {
+        return News::find($id);
+    }
+
 }
